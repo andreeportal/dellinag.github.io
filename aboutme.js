3ee81987ddf57f5d3bc6b88bd24842de0ad44df8
dellinag <41652972+dellinag@users.noreply.github.com>
2018-07-25 16:12:41 -0700

    Update aboutme.js

--- aboutme.js
+++ aboutme.js
@@ -5,3 +5,12 @@ function enlarge(word){
 function smaller(word){
   word.style.fontSize= "10px";
 }
+
+SC.initialize({
+  client_id: 'YOUR_CLIENT_ID'
+});
+
+var track_url = 'https://soundcloud.com/';
+SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
+  console.log('oEmbed response: ', oEmbed);
+});
